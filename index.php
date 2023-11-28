88374c3722b2e3cccb2d85d602d44a5a7a1f3eca
MarcosBenHurSilva <marcosbenhurx@gmail.com>
2023-11-28 00:11:04 -0300

    Bases

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
 <?php
-    echo"I like pizza";
+    echo"Hello world <br>";
+    echo "Hi mom 😆";
+    // Isto é um comentário
+    /* This
+        is
+        a
+        multiline
+        comment
+    */
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <button>order pizza</button>
+</body>
+</html>
